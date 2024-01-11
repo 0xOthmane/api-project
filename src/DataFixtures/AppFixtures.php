82ddefbfcee83926383148781e4f5933d406dd5f
0xOthmane <0xothodor@proton.me>
2024-01-11 15:39:10 +0100

    feat: add user factory

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Factory\TreasureFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,5 +12,6 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         TreasureFactory::createMany(40);
+        UserFactory::createMany(10);
     }
 }
