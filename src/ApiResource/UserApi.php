f3e5c5e0ef89866019d5cd0cd5536c85e8f514c2
0xOthmane <0xothodor@proton.me>
2024-01-15 10:57:31 +0100

    feat: add user dto

---
+++ src/ApiResource/UserApi.php
@@ -0,0 +1,28 @@
+<?php
+
+use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
+use ApiPlatform\Doctrine\Orm\State\Options;
+use ApiPlatform\Metadata\ApiResource;
+use App\Entity\User;
+use App\State\EntityToDtoStateProvider;
+
+#[ApiResource(
+    shortName: 'User',
+    // provider: CollectionProvider::class
+    provider: EntityToDtoStateProvider::class,
+    paginationItemsPerPage: 5,
+    stateOptions: new Options(entityClass: User::class)
+)]
+class UserApi
+{
+    public ?int $id = null;
+
+    public ?string $email = null;
+
+    public ?string $username = null;
+
+    /**
+     * @var array<int, DragonTreasure>
+     */
+    public array $dragonTreasures = [];
+}
