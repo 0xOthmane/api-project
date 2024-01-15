f5fc9d5003527c86fde947961f1dd7005338e587
0xOthmane <0xothodor@proton.me>
2024-01-15 15:00:09 +0100

    feat: add conditional field strategies

--- src/ApiResource/UserApi.php
+++ src/ApiResource/UserApi.php
@@ -2,10 +2,13 @@
 
 use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
 use ApiPlatform\Doctrine\Orm\State\Options;
+use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use App\Entity\User;
 use App\State\EntityClassDtoStateProcessor;
 use App\State\EntityToDtoStateProvider;
+use Symfony\Component\Serializer\Attribute\Ignore;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 #[ApiResource(
     shortName: 'User',
@@ -13,22 +16,29 @@ use App\State\EntityToDtoStateProvider;
     provider: EntityToDtoStateProvider::class,
     processor: EntityClassDtoStateProcessor::class,
     paginationItemsPerPage: 5,
-    stateOptions: new Options(entityClass: User::class)
+    stateOptions: new Options(entityClass: User::class),
+    // normalizationContext: [AbstractNormalizer::IGNORED_ATTRIBUTES => ['throwingDistance']],
+    // denormalizationContext: [AbstractNormalizer::IGNORED_ATTRIBUTES => ['throwingDistance']],
 )]
 class UserApi
 {
+    #[ApiProperty(readable: false, writable: false, identifier: true)]
     public ?int $id = null;
 
     public ?string $email = null;
 
     public ?string $username = null;
 
+    #[ApiProperty(readable: false)]
     public ?string $password = null;
 
     /**
      * @var array<int, Treasure>
      */
+    #[ApiProperty(writable: false)]
     public array $treasures = [];
-
+    
+    // #[ApiProperty(readable: false, writable: false)]
+    #[ApiProperty(writable: false)]
     public int $throwingDistance = 0;
 }
