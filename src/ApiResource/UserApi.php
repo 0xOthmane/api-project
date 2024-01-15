92fa31a571cf8b2c1c658a3d3f209c733fe92276
0xOthmane <0xothodor@proton.me>
2024-01-15 15:15:39 +0100

    feat: add validation and constraints

--- src/ApiResource/UserApi.php
+++ src/ApiResource/UserApi.php
@@ -9,6 +9,12 @@ use App\State\EntityClassDtoStateProcessor;
 use App\State\EntityToDtoStateProvider;
 use Symfony\Component\Serializer\Attribute\Ignore;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ApiResource(
     shortName: 'User',
@@ -19,17 +25,34 @@ use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
     stateOptions: new Options(entityClass: User::class),
     // normalizationContext: [AbstractNormalizer::IGNORED_ATTRIBUTES => ['throwingDistance']],
     // denormalizationContext: [AbstractNormalizer::IGNORED_ATTRIBUTES => ['throwingDistance']],
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(
+            security: 'is_granted("PUBLIC_ACCESS")',
+            validationContext: ['groups' => ['Default', 'postValidation']]
+        ),
+        new Patch(
+            security: 'is_granted("ROLE_USER_EDIT")'
+        ),
+        new Delete(),
+    ],
+    security: 'is_granted("ROLE_USER")',
 )]
 class UserApi
 {
     #[ApiProperty(readable: false, writable: false, identifier: true)]
     public ?int $id = null;
 
+    #[Assert\NotBlank]
+    #[Assert\Email]
     public ?string $email = null;
 
+    #[Assert\NotBlank]
     public ?string $username = null;
 
     #[ApiProperty(readable: false)]
+    #[Assert\NotBlank(groups: ['postValidation'])]
     public ?string $password = null;
 
     /**
@@ -37,7 +60,7 @@ class UserApi
      */
     #[ApiProperty(writable: false)]
     public array $treasures = [];
-    
+
     // #[ApiProperty(readable: false, writable: false)]
     #[ApiProperty(writable: false)]
     public int $throwingDistance = 0;
