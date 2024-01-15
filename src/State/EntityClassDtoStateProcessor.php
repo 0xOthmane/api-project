e29d0b3bcf0cede095ff6e8147a3d535cf9a276b
0xOthmane <0xothodor@proton.me>
2024-01-15 13:10:47 +0100

    feat: add persist and remove processors

--- src/State/EntityClassDtoStateProcessor.php
+++ src/State/EntityClassDtoStateProcessor.php
@@ -2,25 +2,39 @@
 
 namespace App\State;
 
+use ApiPlatform\Doctrine\Common\State\PersistProcessor;
+use ApiPlatform\Doctrine\Common\State\RemoveProcessor;
+use ApiPlatform\Metadata\DeleteOperationInterface;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use UserApi;
 
 class EntityClassDtoStateProcessor implements ProcessorInterface
 {
 
     public function __construct(
+        #[Autowire(service: PersistProcessor::class)] private ProcessorInterface $persistProcessor,
+        #[Autowire(service: RemoveProcessor::class)] private ProcessorInterface $removeProcessor,
+
         private UserRepository $userRepository
     ) {
     }
-    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
     {
         // Handle the state
         // dd($data);
         assert($data instanceof UserApi);
         $entity = $this->mapDtoToEntity($data);
+        if ($operation instanceof DeleteOperationInterface) {
+            $this->removeProcessor->process($entity, $operation, $uriVariables, $context);
+            return null;
+        }
+        $this->persistProcessor->process($entity, $operation, $uriVariables, $context);
+        $data->id = $entity->getId();
+        return $data;
     }
 
     private function mapDtoToEntity(object $dto): object
