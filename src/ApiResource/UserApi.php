e7752515f5fd17f4457d8559132ecfc8193d9868
0xOthmane <0xothodor@proton.me>
2024-01-15 11:49:03 +0100

    feat add DTO to Entity State Processor

--- src/ApiResource/UserApi.php
+++ src/ApiResource/UserApi.php
@@ -4,12 +4,14 @@ use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
 use ApiPlatform\Doctrine\Orm\State\Options;
 use ApiPlatform\Metadata\ApiResource;
 use App\Entity\User;
+use App\State\EntityClassDtoStateProcessor;
 use App\State\EntityToDtoStateProvider;
 
 #[ApiResource(
     shortName: 'User',
     // provider: CollectionProvider::class
     provider: EntityToDtoStateProvider::class,
+    processor: EntityClassDtoStateProcessor::class,
     paginationItemsPerPage: 5,
     stateOptions: new Options(entityClass: User::class)
 )]
