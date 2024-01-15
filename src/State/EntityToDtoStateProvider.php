83756b21b5adca084a4c721098ee2399ac4b25f3
0xOthmane <0xothodor@proton.me>
2024-01-15 10:58:09 +0100

    feat: add EntityToDtoStateProvider

---
+++ src/State/EntityToDtoStateProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
+class EntityToDtoStateProvider implements ProviderInterface
+{
+    public function __construct(
+        #[Autowire(service: CollectionProvider::class)] private ProviderInterface $collectionProvider
+    ) {
+    }
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
+    {
+        // Retrieve the state from somewhere
+        $entities = $this->collectionProvider->provide($operation, $uriVariables, $context);
+        // dd($entities);
+        dd(iterator_to_array($entities));
+    }
+}
